b0854cba35b4e7dd772b17741f3ce29fd11c9552
Krist10an <krist10an@hotmail.com>
2015-10-18 14:04:09 +0200

    Add capacity

--- app.js
+++ app.js
@@ -12,17 +12,42 @@ app.config(function($routeProvider) {
 });
 
 
-// -- Controllers --
-
-app.controller("ElbilKalkController", function($scope) {
-    $scope.carPresets = [
-        { "name" : "Model S", "consumption" : 17, "battery": 85},
-        { "name" : "eGolf", "consumption" : 17, "battery" : 21.4},
-        ];
-    $scope.orderProp = 'name';
+app.factory('CarCapacity', function() {
+    var data = {
+        Capacity: 21.2,
+    };
+    return {
+        getCapacity: function() {
+            return data.Capacity;
+        },
+        setCapacity: function(value) {
+            console.log("Setting capacity to", value);
+            data.Capacity = value;
+        }
+    }
 });
 
-app.controller("ConsumptionController", function($scope) {
+// -- Controllers --
+
+app.controller("ElbilKalkController", function($scope, CarCapacity) {
+    $scope.carPresets = [
+        { "name" : "Tesla Model S", "consumption" : 17, "battery": 85},
+        { "name" : "VW eGolf", "consumption" : 17, "battery" : 21.4},
+        ];
+    $scope.orderProp = 'name';
+    $scope.CarCapacity = CarCapacity
+    $scope.currentCapacity = CarCapacity.getCapacity();
+
+    $scope.$watch('currentCapacity', function (newValue, oldValue) {
+        if (newValue !== oldValue) CarCapacity.setCapacity(newValue);
+    });
+    $scope.setCapacity = function(value) {
+        $scope.currentCapacity = value;
+    };
+});
+
+app.controller("ConsumptionController", function($scope, CarCapacity) {
+    $scope.carCapacity = CarCapacity;
     $scope.estimated = 0;
     $scope.car = { "capacity": 21.2, "soc": 100, "consumption": 15.5 }
     $scope.consumptionUnits = [ "kWh/100km"];
@@ -36,10 +61,10 @@ app.controller("ConsumptionController", function($scope) {
 
     // Set initial values
     $scope.calculate();
-
 });
 
-app.controller("DistanceController", function($scope) {
+app.controller("DistanceController", function($scope, CarCapacity) {
+    $scope.carCapacity = CarCapacity;
     $scope.estimated = 0;
     $scope.car = { "capacity": 21.2, "soc": 100 }
     $scope.distance = 150;
