c2aff5194a70c271d97aa65a63c48adf659b5d94
Krist10an <krist10an@hotmail.com>
2015-10-21 22:12:49 +0200

    Just to see what pages are actually loaded and not in the stats

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ var app = angular.module("ElbilKalkApp", [
     "ionic.utils",
 ]);
 
+app.run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+ $rootScope
+    .$on('$routeChangeSuccess',
+        function(event){
+            if (!$window.ga)
+                return;
+            $window.ga('send', 'pageview', { page: $location.path() });
+    });
+}])
 // Routes
 
 app.config(function($routeProvider) {
